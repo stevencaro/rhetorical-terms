2164d05ad0d388b6d3931d0cd7045531f3648a4f
Steven Caro <stevencaro@gmail.com>
2018-07-23 18:09:12 -0700

    added previous functionality with ArrayStack. needs refactoring.

--- showrhetoric.py
+++ showrhetoric.py
@@ -4,38 +4,56 @@ import sys
 
 import urwid as ur
 
+from ArrayStack import ArrayStack
 from rhetoric import Rhetoric
 
-def unhandled_input(key):
-    if key in ['Q', 'q', 'esc']:
-        raise ur.ExitMainLoop()
-    elif key in ['n', 'space']:
-        main.contents['body'] = (getbody(), None)
-    else:
-        pass
-    return True
+seen_terms = ArrayStack()
+prev_term_g = ()
+curr_term_g = ()
 
-def getterm(term=''):
+def getterm():
     ### instantiates rhetoric object
     r = Rhetoric()
 
     term, gloss, examples = r.term()
 
-    ### format content for display
+
+    return term, gloss, examples
+
+def prev_term():
+    t, g, e = seen_terms.pop()
+    return t,g,e
+
+def getbody( term, gloss, examples ):
+    global curr_term_g
+    curr_term_g = (term, gloss, examples)
     content = [ ur.Text(''),
                 ur.Text(('term', term), align = 'center'),
                 ur.Text(''),
                 ur.Text(('gloss', gloss), align = 'center'),
                 ur.Text('') ] + [ ur.Text(('ex', ex + '\n'), align = 'left') for ex in examples ]
 
-    return content
+    return ur.Filler( ur.Padding( ur.LineBox(ur.Pile( content ) ), left=10, right=10))
 
-def getbody():
-    return ur.Filler( ur.Padding( ur.LineBox(ur.Pile( getterm()) ), left=10, right=10))
-
+def unhandled_input(key):
+    #global prev_term, seen_terms, curr_term
+    if key in ['Q', 'q', 'esc']:
+        raise ur.ExitMainLoop()
+    elif key in ['n', 'space']:
+        prev_term_g = curr_term_g
+        seen_terms.push( prev_term_g )
+        main.contents['body'] = (getbody(*getterm()), None)
+    elif key in ['p']:
+        if seen_terms.is_empty():
+            pass
+        else:
+            main.contents['body'] = (getbody(*prev_term()), None)
+    else:
+        pass
+    return True
 ### Main application
 if __name__ == '__main__':
-    body = getbody()
+    body = getbody(*getterm())
 
     palette = [ ('term',     'light blue',  'black' ),
                 ('gloss',    'yellow',      'black' ),
